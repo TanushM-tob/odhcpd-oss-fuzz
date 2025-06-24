a0316d2580205093b0ca52c55eb8788748cc45a9
tarun <tarun@taruns-MacBook-Pro.local>
2025-06-23 23:12:37 -0400

    back to static

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -eu
 
-
 apt-get update
 apt-get install -y build-essential cmake pkg-config git libjson-c-dev
 
@@ -8,117 +7,72 @@ DEPS_DIR="$PWD/deps"
 mkdir -p "$DEPS_DIR"
 cd "$DEPS_DIR"
 
-if [ ! -d "libubox" ]; then
-    echo "Downloading libubox..."
-    git clone https://github.com/openwrt/libubox.git
+if [ ! -d libubox ]; then
+  git clone https://github.com/openwrt/libubox.git
 fi
+cmake -S libubox -B libubox/build \
+  -DCMAKE_INSTALL_PREFIX="$DEPS_DIR/install" \
+  -DCMAKE_C_FLAGS="$CFLAGS" \
+  -DBUILD_STATIC=ON \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_LUA=OFF \
+  -DBUILD_EXAMPLES=OFF
+cmake --build libubox/build --target install -j"$(nproc)"
 
-cd libubox
-mkdir -p build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="$DEPS_DIR/install" \
-         -DCMAKE_C_FLAGS="$CFLAGS" \
-         -DBUILD_LUA=OFF \
-         -DBUILD_EXAMPLES=OFF \
-         -DBUILD_STATIC=ON \
-         -DBUILD_SHARED_LIBS=OFF
-make -j$(nproc)
-make install
-cd "$DEPS_DIR"
-
-if [ ! -d "uci" ]; then
-    echo "Downloading libuci..."
-    git clone https://git.openwrt.org/project/uci.git
+if [ ! -d uci ]; then
+  git clone https://git.openwrt.org/project/uci.git
 fi
+cmake -S uci -B uci/build \
+  -DCMAKE_INSTALL_PREFIX="$DEPS_DIR/install" \
+  -DCMAKE_C_FLAGS="$CFLAGS" \
+  -DBUILD_STATIC=ON \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_LUA=OFF
+cmake --build uci/build --target install -j"$(nproc)"
+cp uci/build/libuci.a "$DEPS_DIR/install/lib/"
 
-cd uci
-mkdir -p build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="$DEPS_DIR/install" \
-         -DCMAKE_C_FLAGS="$CFLAGS" \
-         -DBUILD_LUA=OFF \
-         -DBUILD_SHARED_LIBS=OFF
-make -j$(nproc)
-make install
-cd "$DEPS_DIR"
-
-if [ ! -d "libnl-tiny" ]; then
-    echo "Downloading libnl-tiny..."
-    git clone https://git.openwrt.org/project/libnl-tiny.git
+if [ ! -d libnl-tiny ]; then
+  git clone https://git.openwrt.org/project/libnl-tiny.git
 fi
+cmake -S libnl-tiny -B libnl-tiny/build \
+  -DCMAKE_INSTALL_PREFIX="$DEPS_DIR/install" \
+  -DCMAKE_C_FLAGS="$CFLAGS" \
+  -DBUILD_SHARED_LIBS=OFF
+cmake --build libnl-tiny/build --target install -j"$(nproc)"
 
-cd libnl-tiny
-mkdir -p build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="$DEPS_DIR/install" \
-         -DCMAKE_C_FLAGS="$CFLAGS" \
-         -DBUILD_SHARED_LIBS=OFF
-make -j$(nproc)
-make install
-cd "$DEPS_DIR"
-
-if [ ! -d "ubus" ]; then
-    echo "Downloading libubus..."
-    git clone https://git.openwrt.org/project/ubus.git
+if [ ! -d ubus ]; then
+  git clone https://git.openwrt.org/project/ubus.git
 fi
-
-cd ubus
-mkdir -p build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="$DEPS_DIR/install" \
-         -DCMAKE_C_FLAGS="$CFLAGS" \
-         -DBUILD_LUA=OFF \
-         -DBUILD_EXAMPLES=OFF \
-         -DBUILD_SHARED_LIBS=OFF
-make -j$(nproc)
-make install
-cd "$DEPS_DIR"
-
-cd ..
+cmake -S ubus -B ubus/build \
+  -DCMAKE_INSTALL_PREFIX="$DEPS_DIR/install" \
+  -DCMAKE_C_FLAGS="$CFLAGS" \
+  -DBUILD_STATIC=ON \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_LUA=OFF \
+  -DBUILD_EXAMPLES=OFF
+cmake --build ubus/build --target install -j"$(nproc)"
 
 : "${CFLAGS:=-O2 -fPIC}"
 : "${LDFLAGS:=}"
-: "${PKG_CONFIG_PATH:=}"
-: "${LIB_FUZZING_ENGINE:=-fsanitize=fuzzer}"  # Default to libFuzzer if not provided
-
-# Add flag to suppress C23 extension warnings
+: "${LIB_FUZZING_ENGINE:=-fsanitize=fuzzer}"
 export CFLAGS="$CFLAGS -Wno-c23-extensions"
-
 export PKG_CONFIG_PATH="$DEPS_DIR/install/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
 export CFLAGS="$CFLAGS -I$DEPS_DIR/install/include"
-export LDFLAGS="$LDFLAGS -L$DEPS_DIR/install/lib"
-
-export CFLAGS="$CFLAGS -D_GNU_SOURCE -DDHCPV4_SUPPORT -DWITH_UBUS -std=gnu99"
 export CFLAGS="$CFLAGS -I$DEPS_DIR/install/include/libnl-tiny"
+export LDFLAGS="$LDFLAGS -L$DEPS_DIR/install/lib"
+export CFLAGS="$CFLAGS -D_GNU_SOURCE -DDHCPV4_SUPPORT -DWITH_UBUS -std=gnu99"
 
-cd src
+cd "$PWD/../src"
+for f in odhcpd.c config.c router.c dhcpv6.c ndp.c dhcpv6-ia.c dhcpv6-pxe.c netlink.c dhcpv4.c ubus.c; do
+  $CC $CFLAGS -c "$f" -o "${f%.c}.o"
+done
+$CC $CFLAGS -c "../fuzz_odhcpd.c" -o fuzz_odhcpd.o
 
-echo "Compiling odhcpd source files..."
-$CC $CFLAGS -c odhcpd.c -o odhcpd.o
-$CC $CFLAGS -c config.c -o config.o
-$CC $CFLAGS -c router.c -o router.o
-$CC $CFLAGS -c dhcpv6.c -o dhcpv6.o
-$CC $CFLAGS -c ndp.c -o ndp.o
-$CC $CFLAGS -c dhcpv6-ia.c -o dhcpv6-ia.o
-$CC $CFLAGS -c dhcpv6-pxe.c -o dhcpv6-pxe.o
-$CC $CFLAGS -c netlink.c -o netlink.o
-$CC $CFLAGS -c dhcpv4.c -o dhcpv4.o
-$CC $CFLAGS -c ubus.c -o ubus.o
-
-cd ..
-
-echo "Compiling fuzzer..."
-$CC $CFLAGS -c fuzz_odhcpd.c -o fuzz_odhcpd.o
-
-echo "Linking fuzzer statically..."
 $CC $CFLAGS $LIB_FUZZING_ENGINE fuzz_odhcpd.o \
-    src/odhcpd.o src/config.o src/router.o src/dhcpv6.o src/ndp.o \
-    src/dhcpv6-ia.o src/dhcpv6-pxe.o src/netlink.o src/dhcpv4.o \
-    src/ubus.o \
-    $LDFLAGS -static -lubox -luci -lnl-tiny -lresolv -ljson-c -lubus \
-    -o $OUT/odhcpd_fuzzer
-rm -f *.o src/*.o
+  odhcpd.o config.o router.o dhcpv6.o ndp.o dhcpv6-ia.o dhcpv6-pxe.o netlink.o dhcpv4.o ubus.o \
+  $LDFLAGS -lubox -luci -lubus -lnl-tiny -ljson-c -lresolv \
+  -o "$OUT/odhcpd_fuzzer"
 
+rm -f *.o
 echo "Build completed successfully!"
 echo "Fuzzer binary: $OUT/odhcpd_fuzzer"
-
